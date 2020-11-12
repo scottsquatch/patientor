08d661a8a25cdd6c9051c4a68ef58e3d1e3cd446
Scott Dose <sdose27@gmail.com>
2020-11-11 17:18:41 -0800

    Complete exercise 9.17

---
+++ src/PatientPage/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useStateValue } from '../state';
+import { Header, Icon } from 'semantic-ui-react';
+import axios from 'axios';
+import { Patient } from '../types';
+import { apiBaseUrl } from '../constants';
+
+const PatientPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [ patient, setPatient ] = React.useState<Patient | undefined>();
+  const [{ patients }, dispatch] = useStateValue();
+  React.useEffect(() => {
+    const fetchPatientInfo = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(`${apiBaseUrl}/patients/${id}`);
+        dispatch({ type: 'UPDATE_PATIENT', payload: patientFromApi });
+        setPatient(patientFromApi);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    const existingPatient = patients[id];
+    if (!existingPatient || !existingPatient.ssn) {
+      fetchPatientInfo();
+    } else {
+      setPatient(existingPatient);
+    }
+  }, [dispatch, patients, id]);
+
+  if (!patient) {
+    return (<div>loading...</div>);
+  }
+
+  const GenderIcon: React.FC = () => {
+    switch (patient.gender) {
+      case "male":
+        return <><Icon name="mars" /></>;
+      case "female":
+        return <><Icon name="venus" /></>;
+      case "other":
+        return <><Icon name="genderless" /></>;
+      default:
+        return null;
+    }
+  };
+  return (<div>
+    <Header as="h2">{patient.name}
+      <GenderIcon /></Header>
+    <p>
+      ssn: {patient.ssn}<br />
+      occupation: {patient.occupation}<br />
+      date of birth: {patient.dateOfBirth}
+    </p>
+  </div>);
+};
+
+export default PatientPage;
